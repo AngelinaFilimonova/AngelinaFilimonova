b2761a3acc2f1215f02935bc92962e3ab76a2c34
Ангелина Филимонова <angelinafilimonova@MacBook-Air-Angelina.local>
2024-10-05 16:13:04 +0400

    First commit to git

---
+++ HelloWorld2/src/Cikls/BreakCont1.java
@@ -0,0 +1,30 @@
+package Cikls;
+
+import java.util.Scanner;
+
+public class BreakCont1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        int count = 0;
+        if (num == 1){
+            System.out.println("NO");
+            return;
+        }
+        int i = 0;
+        while (i < num) {
+            i++;
+            if (num % i == 0) {
+                count++;
+            }
+
+            if (count > 2) {
+                System.out.println("NO");
+                break;
+            }
+        }
+        if (count == 2){
+            System.out.println("YES");
+        }
+    }
+}
